f7e5243ea279aee73b4d6bcd2baa09762039e8c1
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 01:31:36 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,8 @@ git clone https://github.com/Lucky-graf/lamp-instul.git
 bash lamp_inst.sh
 echo Now mount the site
 git clone https://github.com/Lucky-graf/mysite.git
-mv mysite /var/www/mysite 
+mv mysite /var/www/site-car 
 sudo systemctl restart apache2
 sudo systemctl status apache2
-echo -----------!!!Congratulations!!!-----------------
-echo !!!Your server is working fine and feeling good!!!
+echo '-----------!!!Congratulations!!!-----------------'
+echo '!!!Your server is working fine and feeling good!!!'
