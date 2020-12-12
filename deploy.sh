2f5eedc9291c2a5dbf71fb34c0248db09fb8cf64
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 01:12:46 +0200

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!bin/bash/
+
+echo Hi, I was created to deploy a term paper Boyko Bogdan
+echo --Start deploy environment--
+echo Deployment started LAMP Server
+git clone https://github.com/Lucky-graf/lamp-instul.git
+bash lamp_inst.sh
+echo Now mount the site
+git clone https://github.com/Lucky-graf/mysite.git
+mv mysite /var/www/mysite 
+sudo systemctl restart apache2
+sudo systemctl status apache2
+echo -----------!!!Congratulations!!!-----------------
+echo !!!Your server is working fine and feeling good!!!
