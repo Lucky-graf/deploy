0967b9e6b142a128619434ab9a52809485d215e7
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 01:52:50 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,11 @@
 echo Hi, I was created to deploy a term paper Boyko Bogdan
 echo --Start deploy environment--
 echo Deployment started LAMP Server
+sleep 5s
 git clone https://github.com/Lucky-graf/lamp-instul.git
 bash lamp_inst.sh
 echo Now mount the site
+sleep 5s
 git clone https://github.com/Lucky-graf/mysite.git
 sudo mv mysite /var/www/site-car 
 sudo systemctl restart apache2
