90939dec3b59030d558f2c939aa99f8c353481f6
Bodya <57416546+Lucky-graf@users.noreply.github.com>
2020-12-13 07:21:10 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,20 +2,20 @@
 times=$(date +%s)
 echo Hi, I was created to deploy a term paper Boyko Bogdan
 echo --Start deploy environment--
-echo Deployment started LAMP Server
-sleep 5
-git clone https://github.com/Lucky-graf/lamp-instul.git
-cd lamp-instul
-bash lamp_inst.sh
 echo Now mount the site
 sleep 5
 git clone https://github.com/Lucky-graf/site-car.git
 sudo mkdir /var/www/
 sudo mv /home/graf/deploy/site-car /var/www/
-sudo systemctl restart apache2
-sudo systemctl status apache2
+echo Deployment started LAMP Server
+sleep 5
+git clone https://github.com/Lucky-graf/lamp-instul.git
+cd lamp-instul
+bash lamp_inst.sh
 echo '-----------!!!Congratulations!!!-----------------'
 echo '!!!Your server is working fine and feeling good!!!'
 sudo rm -rf /home/graf/deploy
 #sudo rmdir -R lamp-instul
 echo 'This operation took-' $(($(date +%s)-$times)) '-time!'
+echo Myip: 
+ip a
